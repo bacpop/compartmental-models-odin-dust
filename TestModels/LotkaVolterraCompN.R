120f889da68098ae9c9108a79d27e38aeb47946b
LeonieLorenz <leonie.lorenz96@web.de>
2023-03-01 15:30:27 +0000

    Lotka Volterra N model with different parameter values that cause negative probabilies

--- TestModels/LotkaVolterraCompN.R
+++ TestModels/LotkaVolterraCompN.R
@@ -7,18 +7,29 @@ update(time) <- (step + 1) * dt
 
 ## Core equations for transitions between compartments:
 # C is the vector of model compartments. Its dimension is determined by the input of the user (species_no)
-update(C[]) <- C[i] - n_death[i] + n_birth[i]
+#update(C[]) <- C[i] - n_change[i]
+update(C[]) <- C[i] - n_birth[i] - n_death[i]
 
 ## Individual probabilities of transition:
 alpha_pop[,] <- alpha[i, j] * C[j] #calculates the current influences of species, based on competition matrix and current population sizes
 # note to myself: this is a dot product, two for-loops. This is not standard matrix * vector multiplication (otherwise the result would be a vector, not a matrix)
 
-p_birth[] <- 1 - exp(-r_C[i] * dt) # probabilities for birth process
-p_death[] <- 1 - exp(-r_C[i] / K[i] * sum(alpha_pop[i,]) * dt) # probabilities for death processes, including the death process that is part of the logistic growth
 
-## Draws from binomial distributions for births and deaths in each population (=compartment):
-n_birth[] <- rbinom(C[i],p_birth[i])
-n_death[] <- rbinom(C[i],p_death[i]) 
+
+
+p_birth[] <- r_C[i] * C[i]
+p_death[] <- r_C[i]/K[i] * C[i] * sum(alpha_pop[i,])
+
+###### version 1: separating birth and death processes:
+n_death[] <- rpois(p_death[i] * dt)
+n_birth[] <- rpois(p_birth[i] * dt)
+
+##### version 2: now joint draws for births and deaths
+#p_change[] <- p_birth[i] - p_death[i]
+#n_change[] <- rpois(p_change[i] * dt)
+#dim(p_change) <- species_no
+#dim(n_change) <- species_no
+
 
 
 ## Initial states:
@@ -37,8 +48,9 @@ species_no <- user() # number of species
 
 dim(p_birth) <- species_no
 dim(p_death) <- species_no
-dim(n_death) <- species_no
+
 dim(n_birth) <- species_no
+dim(n_death) <- species_no
 dim(alpha) <- c(species_no,species_no)
 dim(alpha_pop) <- c(species_no, species_no)
 
@@ -49,7 +61,15 @@ dim(K) <- species_no
 
 
 
-### alternative version with poisson distribution:
+### alternative version with binomial distribution:
+
+#p_birth[] <- 1 - exp(-r_C[i] * dt) # probabilities for birth process
+#p_death[] <- 1 - exp(-r_C[i] / K[i] * sum(alpha_pop[i,]) * dt) # probabilities for death processes, including the death process that is part of the logistic growth
+
+
+## Draws from binomial distributions for births and deaths in each population (=compartment):
+#n_birth[] <- rbinom(C[i],p_birth[i])
+#n_death[] <- rbinom(C[i],p_death[i]) 
 
 #p_birth[] <- r_C[i] * C[i]
 #p_death[] <- r_C[i]/K[i] * C[i] * sum(alpha_pop[i,])
