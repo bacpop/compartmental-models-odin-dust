4525775649bbd35a11d8d242fd686bcaa9113627
LeonieLorenz <leonie.lorenz96@web.de>
2023-03-13 13:59:50 +0000

    Wright Fisher 1 Allele diploid wokring for constant prob and binomial draws

---
+++ TestModels/WrightFisher_1Allele_diploid.R
@@ -0,0 +1,59 @@
+## Definition of the time-step and output as "time"
+dt <- user(1)
+initial(time) <- 0
+update(time) <- (step + 1) * dt
+
+## Core equation for population (assume constant size here, at first):
+
+# number of individuals with AA and AB are determined by drawing from Multinom(n, [p1,p2])
+# with n=pop_size and p1 = (p + 1/2q) + (1/2 * (p + 1/2 q)) and p2 = (r + 1/2 q) + (1/2 * (p+q+r)) + (p + 1/2 q)
+# the number of BB individuals is determined by pop_size - number of AA - number of AB
+
+#probAA <- (AA / pop_size + 1/2 * (AB / pop_size)) + (1/2 * (AA / pop_size + 1/2 * (AB / pop_size)))
+#probAB <- (BB / pop_size + 1/2 * (AB / pop_size)) + (1/2 * (AA + AB + BB) / pop_size) + (AA / pop_size + 1/2 * (AB / pop_size))
+#probAA <- .2
+#probAB <- .4
+
+p[1] <- .2
+p[2] <- .8
+y[1] <- rbinom(pop_size, p[1])
+y[2] <- rbinom(pop_size - y[1], p[2])
+
+#update(x[]) <- y[i]
+#dim(x) <- 2
+#y[] <- rmultinom(pop_size, p)
+# initial(x[]) <- initx[i]
+# initx[] <- user()
+# dim(initx) <- 2
+dim(p) <- 2
+dim(y) <- 2
+
+
+#multiProb[1] <- 0.2
+#multiProb[2] <- 0.8
+#nextGen[] <- rmultinom(10,multiProb)
+
+
+#nextGen[] <- rmultinom(pop_size, multiProb)
+
+update(AA) <- y[1]
+update(AB) <- y[2]
+update(BB) <- pop_size - AA - AB # all individuals that are not AA or AB must be BB
+
+
+## Initial states:
+initial(AA) <- Pop_ini[1] # deterministic, user-based start value
+initial(AB) <- Pop_ini[2] # deterministic, user-based start value
+initial(BB) <- Pop_ini[3]
+
+
+
+
+## User defined parameters - default in parentheses:
+dim(Pop_ini) <- 3
+Pop_ini[] <- user() 
+
+#dim(multiProb) <- 2
+#dim(nextGen) <- 2
+pop_size <- user(100) # population size
+#gen <- user() # number of generations to be simulated
