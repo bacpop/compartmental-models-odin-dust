85f30987348c91e18d0e5b87af23e3006e96a940
LeonieLorenz <leonie.lorenz96@web.de>
2023-03-20 16:28:41 +0000

    implementation of Wright Fisher for n Genotypes including a fitness function (that is based on the genotypes and not on individual genes at the moment though)

--- TestModels/WrightFisher_4Genotypes_haploid.R
+++ TestModels/WrightFisher_4Genotypes_haploid.R
@@ -5,47 +5,40 @@ update(time) <- (step + 1) * dt
 
 ## Core equation for population (assume constant size here, at first):
 
-# number of individuals with Genotype A is determined by drawing from Bin(n, p)
-# with n=pop_size and p = A/pop_size (relative abundance of Genotype A in the population)
 
 p1 <-  A/pop_size
 p2 <- B/pop_size
 p3 <- C/pop_size
 p4 <- D/pop_size
-p_tot <- p1 + p2 + p3 + p4
-y1 <- rbinom(pop_size, p1/p_tot)
-p_tot1 <- p_tot - p1
-y2 <- rbinom(pop_size-y1, (p2/(p2+p3+p4)))
-p_tot2 <- p_tot - p1 - p2
-y3 <- rbinom(pop_size-y1-y2, p3/(p3+p4))
+
+
+# number of individuals with Genotype A is determined by drawing from Bin(n, p)
+# with n=pop_size and p = A/pop_size (relative abundance of Genotype A in the population)
+y1 <- if (p1/(p1+p2+p3+p4) < 1)
+  rbinom(pop_size, p1/(p1+p2+p3+p4)) else pop_size
+
+# The n and p for the remaining binomial draws have to be adapted
+# because we only want to draw (n-x), i.e. those individuals that are not yet determined to be of Genotype A (and then B, C)
+# and we need to adapt the probabilities accordingly to keep the population size the same
+
+y2 <- if (p2/(p2+p3+p4) < 1)
+  rbinom(pop_size - y1, p2/(p2+p3+p4)) else pop_size - y1
+
+y3 <- if (p3/(p3+p4) < 1)
+  rbinom(pop_size -y1 -y2, p3/(p3+p4)) else pop_size -y1 - y2
+
 y4 <- pop_size - y1 - y2 - y3
-### currently issue with random draws
-### by re-scaling p it can sometimes become (slightly) bigger than 1!
-### not sure why this is not an issue for them:
-### https://github.com/mrc-ide/dust/blob/master/inst/include/dust/random/multinomial.hpp
-
-
-#n_inf[2:4] <- rbinom(pop_size - sum(n_inf[1:(i - 1)]), p_inf[i])
 
 update(A) <- y1
 update(B) <- y2
 update(C) <- y3
 update(D) <- y4
-#update(p1) <- y1/pop_size
-#update(p2) <- y2/pop_size
-#update(p3) <- y3/pop_size
-#update(p4) <- y4/pop_size
 
 ## Initial states:
 initial(A) <- pop_size * A_ini # deterministic, user-based start value
 initial(B) <- pop_size * B_ini # deterministic, user-based start value
 initial(C) <- pop_size * C_ini # deterministic, user-based start value
 initial(D) <- pop_size * D_ini # deterministic, user-based start value
-#initial(p1) <- A_ini
-#initial(p2) <- B_ini
-#initial(p3) <- C_ini
-#initial(p4) <- D_ini
-
 
 ## User defined parameters - default in parentheses:
 A_ini <- user(.25) # initial frequency of Genotype A
---
+++ TestModels/WrightFisher_nGenotypes_haploid.R
@@ -0,0 +1,45 @@
+## Definition of the time-step and output as "time"
+dt <- user(1)
+initial(time) <- 0
+update(time) <- (step + 1) * dt
+
+# calculate probabilities based on relative sizes of groups and their fitness (does not necessarily sum to 1):
+probs[] <- (1 + fitness[i]) * Pop[i]/pop_size 
+
+# number of individuals with Genotype 1 is determined by drawing from Bin(n, p)
+# with n=pop_size and p = probs[1]/sum(probs) (relative abundance of Genotype 1 in the population)
+# the probabilities need to be normalised because of the fitness term 
+y[1] <- if (probs[1]/sum(probs[1:species_no]) < 1)
+      rbinom(pop_size, probs[1]/sum(probs[1:species_no])) else pop_size
+
+# The n and p for the remaining binomial draws have to be adapted
+# because we only want to draw (n-x), i.e. those individuals that are not yet determined to be of Genotype 1 (and then 2, 3 and so on)
+# and we need to adapt the probabilities accordingly to keep the population size the same
+
+y[2:(species_no-1)] <- if (probs[i]/sum(probs[i:species_no]) < 1)
+  rbinom(pop_size - sum(y[1:(i-1)]), probs[i]/sum(probs[i:species_no])) else pop_size - sum(y[1:(i-1)])
+
+# the last species will get all individuals that aren't assigned to a Genotype yet
+y[species_no] <- pop_size - sum(y[1:(species_no-1)])
+
+## Core equation for population (assume constant size here):
+update(Pop[]) <- y[i] 
+
+## Initial states:
+Pop_ini_norm[] <- Pop_ini[i]/sum(Pop_ini[1:species_no]) # normalizing input vector (then it does not matter whether input consists of absolute or relative values)
+# maybe a check that Pop_ini sums to 1 or pop_size would be nice. Not sure how to implement such a check though.
+
+initial(Pop[]) <- as.integer(pop_size * Pop_ini_norm[i]) # deterministic, user-based start value
+
+
+## User defined parameters - default in parentheses:
+species_no <- user()
+Pop_ini[] <- user() # initial frequency of Genotypes
+fitness[] <- user() # fitness vector for different genotypes
+dim(Pop_ini) <- species_no
+dim(Pop_ini_norm) <- species_no
+dim(Pop) <- species_no
+dim(y) <- species_no
+dim(probs) <- species_no
+dim(fitness) <- species_no
+pop_size <- user() # population size
