47644443f44da8916ed0decea603502f6a79a637
LeonieLorenz <leonie.lorenz96@web.de>
2024-04-17 16:50:46 +0100

    re-visiting different functions for NFDS (alternatives to step function and logistic function)

--- WrightFisherModel/gene_freq_changes.R
+++ WrightFisherModel/gene_freq_changes.R
@@ -2,7 +2,7 @@ gene_freq_df_1 <- data.frame(cbind(mass_gene_freq_0/length(which(mass_data$Time=
 colnames(gene_freq_df) <- 1:3
 
 gene_freq_df <- data.frame(x = c(rep(1,length(mass_gene_freq_0)), rep(2,length(mass_gene_freq_0)),rep(3,length(mass_gene_freq_0))),
-                           y = c(mass_gene_freq_0, mass_gene_freq_36, mass_gene_freq_72), 
+                           y = c(mass_gene_freq_0, mass_gene_freq_36, mass_gene_freq_72),
                            z = rep(names(mass_gene_freq_0),3))
 gene_freq_df_test <- rbind(gene_freq_df[1:10,], gene_freq_df[(length(mass_gene_freq_0)+1):(length(mass_gene_freq_0)+10),],gene_freq_df[(2*length(mass_gene_freq_0)+1):(2*length(mass_gene_freq_0)+10),])
 
@@ -24,5 +24,5 @@ for (i in 1:length(mass_gene_freq_0)){
 #}
 
 
-#ggplot(gene_freq_df, aes(y=cluster_sizes, x = as.integer(row.names(df_cluster_sizes)))) + 
+#ggplot(gene_freq_df, aes(y=cluster_sizes, x = as.integer(row.names(df_cluster_sizes)))) +
 #  geom_bar(position="dodge", stat="identity", colour="black", width = 1,  fill="#009E73")
